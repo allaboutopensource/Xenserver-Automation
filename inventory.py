3a5b1aa454c7bf0e64203be2525b5aaf7821e27e
Sunil kathait <5890851+allaboutopensource@users.noreply.github.com>
2020-07-23 14:55:49 +0530

    Update and rename dundee to inventory.py

---
+++ inventory.py
@@ -0,0 +1,13 @@
+xenserver inventory script that runs everyday to list out the name of the vm's running under xenserver host. You need to keep the uuid of the xenserver in the file called "xenserver-host.txt"
+
+
+#!/bin/python
+
+import sys
+import subprocess
+with open("xenserver-host.txt", "r") as host:
+  for line in host:
+     cmd = 'xe vm-list params=name-label resident-on='+line
+     list_vm = subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True)
+     state = list_vm.communicate()[0]
+     print "VM running under xenserver host with the uuid: "+line+"\n", state
